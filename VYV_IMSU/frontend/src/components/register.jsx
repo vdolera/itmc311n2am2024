e6faaad84b90ddcbb98b8c3e02096168aebc1212
Vincent <158262786+vdolera@users.noreply.github.com>
2024-11-25 07:32:06 +0800

    Update register.jsx

--- VYV_IMSU/frontend/src/components/register.jsx
+++ VYV_IMSU/frontend/src/components/register.jsx
@@ -21,7 +21,7 @@ function Signup() {
     setAlertType('');
 
     // Send a POST request with the signup details
-    axios.post('https://imsu-server.vercel.app/register', { email, username, password, userType })
+    axios.post('https://vyv-imsu-server.vercel.app/register', { email, username, password, userType })
       .then(result => {
         console.log(result);
         // Show success message
